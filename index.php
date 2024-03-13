951b1aae5cd3dcd0c024b85425183f3ddcc1d0f3
eduardosaraujo <eduardo.araujo109@etec.sp.gov.br>
2024-03-13 17:28:42 -0300

    qualquer mensagem ai gente

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+$nome = "Eduardo e Pierri";
+$idade = (16+18)/2;
+$altura = (1.74 + 1.69)/2;
+$peso = 75.40;
+
+var_dump($nome);
+echo "<br>";
+var_dump($idade);
+echo "<br>";
+var_dump($altura);
+echo "<br>";
+var_dump($peso);
+echo "<br>";
+
+echo "Eu tenho $altura quilometros de altura";
+echo "<br>";
+echo "<br>";
+echo "Saske Trairaaaaa!!!!";
+
+?>
+</body>
+</html>
