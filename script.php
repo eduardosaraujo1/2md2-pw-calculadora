225c7041cbde03a7b6be55e0d8a2853740dc286f
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-05-22 17:30:58 -0300

    nada a comentar.

--- script.php
+++ script.php
@@ -6,12 +6,19 @@ $numero = $_POST['numero'];
 //contador
 $x = 1;
 
-while ($x <= 10) {
+// while ($x <= 10) {
 
-    $result = $numero * $x;
+//     $result = $numero * $x;
+//     // echo "$numero"."X"."$x"."="."$result";
+//     echo "$numero"."x".$x."=$result";
+//     echo "<br>";
+//     $x++;
+// }
+
+for ($i = 0; $i <= 10; $i++) {
+    $result = $numero * $i;
     // echo "$numero"."X"."$x"."="."$result";
-    echo "$numero"."x".$x."=$result";
+    echo "$numero"."x".$i."=$result";
     echo "<br>";
-    $x++;
 }
 ?>
